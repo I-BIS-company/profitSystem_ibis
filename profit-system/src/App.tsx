b34911027c996ba19c99ea104759eb7800b2cd1e
Kenji <kkawakami391@gmail.com>
2023-07-04 15:51:19 +0900

    Temporal Commit

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -1,5 +1,22 @@
+import { Flex, Link, Divider } from "@chakra-ui/react";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Flex direction="column" width="256px" backgroundColor="orange.50" flex="1">
+      <Link>ログ一覧</Link>
+      <Divider />
+      <Link>工数登録</Link>
+      <Link>ユーザ一覧</Link>
+      <Divider />
+      <Link>顧客一覧</Link>
+      <Divider />
+      <Link>顧客登録</Link>
+      <Link>案件一覧</Link>
+      <Divider />
+      <Link>案件登録</Link>
+      <Link>利益一覧</Link>
+    </Flex>
+  );
 }
 
 export default App;
