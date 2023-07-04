89ff1f2b33c029240729a36763d420e736fec061
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-04 15:56:17 +0900

    react-router-domをインストール

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -1,10 +1,14 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme/theme";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <BrowserRouter>
       <Button>ボタン</Button>
+      </BrowserRouter>
     </ChakraProvider>
   )
 }
