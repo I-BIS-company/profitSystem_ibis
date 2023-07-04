c84e234ca8e9214338f1c853409ef18c08355e50
Kenji <kkawakami391@gmail.com>
2023-07-04 17:08:41 +0900

    Sidebar basic aspect

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -2,19 +2,34 @@ import { Flex, Link, Divider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Flex direction="column" width="256px" backgroundColor="orange.50" flex="1">
-      <Link>ログ一覧</Link>
-      <Divider />
-      <Link>工数登録</Link>
-      <Link>ユーザ一覧</Link>
-      <Divider />
-      <Link>顧客一覧</Link>
-      <Divider />
-      <Link>顧客登録</Link>
-      <Link>案件一覧</Link>
-      <Divider />
-      <Link>案件登録</Link>
-      <Link>利益一覧</Link>
+    <Flex
+      direction="column"
+      width="256px"
+      backgroundColor="orange.50"
+      flex="1"
+      height="100vh"
+      alignItems="center"
+    >
+      <Flex direction="column" alignItems="center" mt="10px" mb="50px">
+        <Link fontSize="20px">ログ一覧</Link>
+        <Divider width="200px" borderColor="BlackAlpha 800" />
+        <Link mt="10px">工数登録</Link>
+      </Flex>
+      <Flex direction="column" alignItems="center" my="10px" mb="50px">
+        <Link fontSize="20px">ユーザ一覧</Link>
+        <Divider width="200px" borderColor="BlackAlpha 800" />
+      </Flex>
+      <Flex direction="column" alignItems="center" my="10px" mb="50px">
+        <Link fontSize="20px">顧客一覧</Link>
+        <Divider width="200px" borderColor="BlackAlpha 800" />
+        <Link mt="10px">顧客登録</Link>
+      </Flex>
+      <Flex direction="column" alignItems="center" my="10px" mb="50px">
+        <Link fontSize="20px">案件一覧</Link>
+        <Divider width="200px" borderColor="BlackAlpha 800" />
+        <Link mt="10px">案件登録</Link>
+        <Link mt="10px">利益一覧</Link>
+      </Flex>
     </Flex>
   );
 }
