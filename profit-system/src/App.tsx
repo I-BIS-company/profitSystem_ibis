a131d60d8c610f01f9ab1752e340d81a83eee645
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-04 21:56:48 +0900

    VIteを使用しているため、main.tsxにChakraProviderをインポート

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -1,16 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
-import { theme } from "./theme/theme";
 import { Router } from "./router/Router";
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
-    </ChakraProvider>
   )
 }
 
