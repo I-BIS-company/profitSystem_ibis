f153d1ecbbed5456ba4dc65a201fb91b5d42564b
Kenji <kkawakami391@gmail.com>
2023-07-04 22:52:41 +0900

    SBItemsContainer component implementation.

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -1,7 +1,7 @@
-import { EditIcon } from "@chakra-ui/icons";
-import { Flex, Link } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { SBMainItem } from "./components/molecules/link/SBMainItem";
 import { SBSubItem } from "./components/molecules/link/SBSubItem";
+import { SBItemsContainer } from "./components/molecules/container/SBItemsContainer";
 
 function App() {
   return (
@@ -12,22 +12,22 @@ function App() {
       height="100vh"
       fontWeight="600"
     >
-      <Flex direction="column" alignItems="center" mt="10px" mb="50px">
+      <SBItemsContainer>
         <SBMainItem text="ログ一覧" />
         <SBSubItem text="工数登録" />
-      </Flex>
-      <Flex direction="column" alignItems="center" my="10px" mb="50px">
+      </SBItemsContainer>
+      <SBItemsContainer>
         <SBMainItem text="ユーザ一覧" />
-      </Flex>
-      <Flex direction="column" alignItems="center" my="10px" mb="50px">
+      </SBItemsContainer>
+      <SBItemsContainer>
         <SBMainItem text="顧客一覧" />
         <SBSubItem text="顧客登録" />
-      </Flex>
-      <Flex direction="column" alignItems="center" my="10px" mb="50px">
+      </SBItemsContainer>
+      <SBItemsContainer>
         <SBMainItem text="案件一覧" />
         <SBSubItem text="案件登録" />
         <SBSubItem text="利益一覧" />
-      </Flex>
+      </SBItemsContainer>
     </Flex>
   );
 }
