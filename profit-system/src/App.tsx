c4668dc5cf21153df4c536e46b08a2bee06ee799
Kenji <kkawakami391@gmail.com>
2023-07-05 01:06:49 +0900

    Sidebar component implementation

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -1,35 +1,7 @@
-import { Flex } from "@chakra-ui/react";
-import { SBMainItem } from "./components/molecules/link/SBMainItem";
-import { SBSubItem } from "./components/molecules/link/SBSubItem";
-import { SBItemsContainer } from "./components/molecules/container/SBItemsContainer";
+import { Sidebar } from "./components/organisms/layout/Sidebar";
 
 function App() {
-  return (
-    <Flex
-      direction="column"
-      width="256px"
-      backgroundColor="orange.50"
-      height="100vh"
-      fontWeight="600"
-    >
-      <SBItemsContainer>
-        <SBMainItem text="ログ一覧" />
-        <SBSubItem text="工数登録" />
-      </SBItemsContainer>
-      <SBItemsContainer>
-        <SBMainItem text="ユーザ一覧" />
-      </SBItemsContainer>
-      <SBItemsContainer>
-        <SBMainItem text="顧客一覧" />
-        <SBSubItem text="顧客登録" />
-      </SBItemsContainer>
-      <SBItemsContainer>
-        <SBMainItem text="案件一覧" />
-        <SBSubItem text="案件登録" />
-        <SBSubItem text="利益一覧" />
-      </SBItemsContainer>
-    </Flex>
-  );
+  return <Sidebar />;
 }
 
 export default App;
