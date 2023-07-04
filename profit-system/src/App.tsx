1976d641953e4d6e848471faa7060a44606f803c
Kenji <kkawakami391@gmail.com>
2023-07-04 18:03:51 +0900

    Add Icons to Sidebar

--- profit-system/src/App.tsx
+++ profit-system/src/App.tsx
@@ -1,3 +1,4 @@
+import { EditIcon } from "@chakra-ui/icons";
 import { Flex, Link, Divider } from "@chakra-ui/react";
 
 function App() {
@@ -6,14 +7,16 @@ function App() {
       direction="column"
       width="256px"
       backgroundColor="orange.50"
-      flex="1"
       height="100vh"
-      alignItems="center"
+      fontWeight="600"
     >
       <Flex direction="column" alignItems="center" mt="10px" mb="50px">
         <Link fontSize="20px">ログ一覧</Link>
         <Divider width="200px" borderColor="BlackAlpha 800" />
-        <Link mt="10px">工数登録</Link>
+        <Link mt="10px">
+          <EditIcon mr="8px" />
+          工数登録
+        </Link>
       </Flex>
       <Flex direction="column" alignItems="center" my="10px" mb="50px">
         <Link fontSize="20px">ユーザ一覧</Link>
@@ -22,13 +25,22 @@ function App() {
       <Flex direction="column" alignItems="center" my="10px" mb="50px">
         <Link fontSize="20px">顧客一覧</Link>
         <Divider width="200px" borderColor="BlackAlpha 800" />
-        <Link mt="10px">顧客登録</Link>
+        <Link mt="10px">
+          <EditIcon mr="8px" />
+          顧客登録
+        </Link>
       </Flex>
       <Flex direction="column" alignItems="center" my="10px" mb="50px">
         <Link fontSize="20px">案件一覧</Link>
         <Divider width="200px" borderColor="BlackAlpha 800" />
-        <Link mt="10px">案件登録</Link>
-        <Link mt="10px">利益一覧</Link>
+        <Link mt="10px">
+          <EditIcon mr="8px" />
+          案件登録
+        </Link>
+        <Link mt="10px">
+          <EditIcon mr="8px" />
+          利益一覧
+        </Link>
       </Flex>
     </Flex>
   );
