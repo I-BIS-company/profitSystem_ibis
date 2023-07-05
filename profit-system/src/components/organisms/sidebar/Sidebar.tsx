9fa41eff58c2e6ac6b3532141b46090e1fd6bfa9
Kenji <kkawakami391@gmail.com>
2023-07-05 17:23:57 +0900

    Change organisms/layout to organisms/sidebar folder, and change import location of components

---
+++ profit-system/src/components/organisms/sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import { Flex } from "@chakra-ui/react";
+import { memo, FC } from "react";
+import { SbItemsContainer } from "../../molecules/container/SbItemsContainer";
+import { SbMainItem } from "../../atoms/item/SbMainItem";
+import { SbSubItem } from "../../atoms/item/SbSubItem";
+
+export const Sidebar: FC = memo(() => {
+  return (
+    <>
+      <Flex
+        direction="column"
+        maxW="256px"
+        backgroundColor="orange.50"
+        height="100vh"
+        fontWeight="600"
+      >
+        <SbItemsContainer>
+          <SbMainItem text="ログ一覧" />
+          <SbSubItem text="工数登録" />
+        </SbItemsContainer>
+        <SbItemsContainer>
+          <SbMainItem text="ユーザ一覧" />
+        </SbItemsContainer>
+        <SbItemsContainer>
+          <SbMainItem text="顧客一覧" />
+          <SbSubItem text="顧客登録" />
+        </SbItemsContainer>
+        <SbItemsContainer>
+          <SbMainItem text="案件一覧" />
+          <SbSubItem text="案件登録" />
+          <SbSubItem text="利益一覧" />
+        </SbItemsContainer>
+      </Flex>
+    </>
+  );
+});
