153d7b72d82bba901af269b8a347f842ff735380
UCDM <palama.ucdm.1212@gmail.com>
2023-07-05 15:23:30 +0900

    add test coment

--- profit-system/src/main.tsx
+++ profit-system/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 
+/* test */
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
