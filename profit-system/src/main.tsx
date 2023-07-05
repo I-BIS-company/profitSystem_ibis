466b0bec38d217180170d64f3a662a7d3dad3d63
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-05 14:14:10 +0900

    main.tsxの不要な空白削除

--- profit-system/src/main.tsx
+++ profit-system/src/main.tsx
@@ -7,7 +7,7 @@ import { theme } from "./theme/theme.ts";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>    
     <ChakraProvider theme={theme}>
-      <App />
+    <App />
     </ChakraProvider>
   </React.StrictMode>
-);
+  );
