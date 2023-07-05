ec775dcafd51f8fd011e436c99b6520c27246bf5
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-05 19:35:20 +0900

    secondarybuttonを追加

--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -1,11 +1,15 @@
 import { FC, memo } from "react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { SecondaryButton } from "../atoms/button/SocondaryButton";
 
 export const Log: FC = memo(() => {
     return (
         <>
         <p>ログページです</p>
         <PrimaryButton Text="登録" />
+        <br />
+        <br />
+        <SecondaryButton Text="編集"/>
         </>
         );
 });
