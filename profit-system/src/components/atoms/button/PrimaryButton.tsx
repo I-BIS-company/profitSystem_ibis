a11b2ae44c6e5fcc262a4bcc1fd379fa09a46e21
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-05 15:15:54 +0900

    PrimaryButtonを作成

---
+++ profit-system/src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@chakra-ui/react";
+import { FC, memo } from "react";
+
+type Props = {
+  Text: string;
+};
+
+export const PrimaryButton: FC<Props> = memo((props) => {
+    const { Text } = props;
+  return (
+    <Button
+        bg="green.400"
+        color="white"
+        size="lg"
+        py={2}
+        px={16}
+        shadow="md"
+        _hover={{opacity:.8}}
+        >
+        {Text}
+    </Button>
+    
+  );
+});
--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -1,5 +1,11 @@
 import { FC, memo } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Log: FC = memo(() => {
-    return <p>ログページです</p>;
+    return (
+        <>
+        <p>ログページです</p>
+        <PrimaryButton Text="登録" />
+        </>
+        );
 });
