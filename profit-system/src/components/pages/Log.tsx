81b4692c5a765ddb22f82717c9e3e5b9bbeba0e0
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-05 20:20:20 +0900

    SocondaryButton=>SecondaryButtonに変更

--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -1,6 +1,6 @@
 import { FC, memo } from "react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
-import { SecondaryButton } from "../atoms/button/SocondaryButton";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Log: FC = memo(() => {
     return (
