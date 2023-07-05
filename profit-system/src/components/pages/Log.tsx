02d40861632fed6ee06cca4aaf1de311faf1e61a
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-05 18:23:49 +0900

    issue#50の追加

--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -3,16 +3,18 @@ import { Sidebar } from "../organisms/Sidebar";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { Flex } from "@chakra-ui/react";
 import { Header } from "../organisms/Header";
+import { MainTemplate } from "../templates/MainTemplate";
 
 export const Log: FC = memo(() => {
     return (
       <>
-      <Header />
+      <MainTemplate>
       <Flex>
         <Sidebar />
         <p>ログページです</p>;
         <PrimaryButton Text="登録" />
       </Flex>
+      </MainTemplate>
       </>
     );
   });
