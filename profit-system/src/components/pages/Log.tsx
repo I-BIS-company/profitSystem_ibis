59810df2a9de9bc2b192694c9c3c812b06e464aa
Kenji <kkawakami391@gmail.com>
2023-07-06 13:17:04 +0900

    Remove Sidebar component.

--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -1,13 +1,11 @@
 import { FC, memo } from "react";
-import { Sidebar } from "../organisms/Sidebar";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Log: FC = memo(() => {
-    return (
-      <>
-        <Sidebar />
-        <p>ログページです</p>;
-        <PrimaryButton Text="登録" />
-      </>
-    );
-  });
+  return (
+    <>
+      <p>ログページです</p>;
+      <PrimaryButton Text="登録" />
+    </>
+  );
+});
