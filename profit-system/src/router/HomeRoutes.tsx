66ab5fc931960036e3dc82484277b4450d870cbf
Kenji <kkawakami391@gmail.com>
2023-07-06 13:12:20 +0900

    Add data for testing routing system.

--- profit-system/src/router/HomeRoutes.tsx
+++ profit-system/src/router/HomeRoutes.tsx
@@ -1,8 +1,18 @@
-import { Login } from "../components/pages/Login";
+import { Log } from "../components/pages/Log";
+import { WorkhourEdit } from "../components/pages/WorkhourEdit";
+import { WorkhourRegister } from "../components/pages/WorkhourRegister";
 
 export const homeRoutes = [
-    {
-        path: "/",
-        element: <Login /> 
-    },
+  {
+    path: "/log",
+    element: <Log />,
+  },
+  {
+    path: "/workhour_register",
+    element: <WorkhourRegister />,
+  },
+  {
+    path: "/workhour_edit",
+    element: <WorkhourEdit />,
+  },
 ];
