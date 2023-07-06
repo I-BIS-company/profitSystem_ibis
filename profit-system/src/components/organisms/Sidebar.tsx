f358c41c1ee46d2c25b8295166734251ec09fde5
Kenji <kkawakami391@gmail.com>
2023-07-06 17:03:47 +0900

    Add path values to items components inside Sidebar

--- profit-system/src/components/organisms/Sidebar.tsx
+++ profit-system/src/components/organisms/Sidebar.tsx
@@ -16,20 +16,20 @@ export const Sidebar: FC = memo(() => {
         fontWeight="600"
       >
         <SbItemsContainer>
-          <SbMainItem text="ログ一覧" />
-          <SbSubItem text="工数登録" />
+          <SbMainItem text="ログ一覧" path="/log" />
+          <SbSubItem text="工数登録" path="/workhour_register" />
         </SbItemsContainer>
         <SbItemsContainer>
-          <SbMainItem text="ユーザ一覧" />
+          <SbMainItem text="ユーザ一覧" path="/users_list" />
         </SbItemsContainer>
         <SbItemsContainer>
-          <SbMainItem text="顧客一覧" />
-          <SbSubItem text="顧客登録" />
+          <SbMainItem text="顧客一覧" path="/companies_list" />
+          <SbSubItem text="顧客登録" path="/company_register" />
         </SbItemsContainer>
         <SbItemsContainer>
-          <SbMainItem text="案件一覧" />
-          <SbSubItem text="案件登録" />
-          <SbSubItem text="利益一覧" />
+          <SbMainItem text="案件一覧" path="/projects_list" />
+          <SbSubItem text="案件登録" path="project_register" />
+          <SbSubItem text="利益一覧" path="profits_list" />
         </SbItemsContainer>
       </Flex>
     </>
