6aae35600558add211958ad4d7cec89ede8c28d1
github kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-06 12:52:27 +0900

    headerのレイアウト追加

---
+++ profit-system/src/components/organisms/Header.tsx
@@ -0,0 +1,25 @@
+import { Flex } from "@chakra-ui/react";
+import { memo, FC } from "react";
+
+
+export const Header: FC = memo(() => {
+  return (
+    <>
+    <Flex
+      width="100%"
+      height="50px"
+      bg="white"
+      justifyContent="space-between"
+      alignItems="center"
+      py={4}
+      px={16}
+    >
+      {/* 左側の要素 */}
+      <div>アイビス株式会社</div>
+
+      {/* 右側の要素 */}
+      <div>右側の要素</div>
+    </Flex>
+    </>
+  );
+});
--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -2,10 +2,12 @@ import { FC, memo } from "react";
 import { Sidebar } from "../organisms/Sidebar";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { Flex } from "@chakra-ui/react";
+import { Header } from "../organisms/Header";
 
 export const Log: FC = memo(() => {
     return (
       <>
+      <Header />
       <Flex>
         <Sidebar />
         <p>ログページです</p>;
