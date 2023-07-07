528b348f9ddbe3c6d19bae81ea0f062d8e526a1c
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-07 11:32:45 +0900

    レビューの修正

--- profit-system/src/components/pages/Log.tsx
+++ profit-system/src/components/pages/Log.tsx
@@ -1,10 +1,7 @@
 import { FC, memo } from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Sidebar } from "../organisms/Sidebar";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { Flex } from "@chakra-ui/react";
-import { Header } from "../organisms/Header";
 import { MainTemplate } from "../templates/MainTemplate";
 
 export const Log: FC = memo(() => {
@@ -20,18 +17,4 @@ export const Log: FC = memo(() => {
       </>
     );
   });
-=======
-import { Sidebar } from "../organisms/sidebar/Sidebar";
-=======
-import { Sidebar } from "../organisms/Sidebar";
->>>>>>> 4834190 (Remove sidebar folder)
 
-export const Log: FC = memo(() => {
-  return (
-    <>
-      <Sidebar />
-      <p>ログページです</p>;
-    </>
-  );
-});
->>>>>>> ae13bd8 (Add Sidebar component to Log page)
