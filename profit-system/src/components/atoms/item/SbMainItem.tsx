b8592e92f20995ece82e500304a2fc7eaa9f2384
Kenji <kkawakami391@gmail.com>
2023-07-09 19:27:21 +0900

    Add current page background color in Sidebar items.

--- profit-system/src/components/atoms/item/SbMainItem.tsx
+++ profit-system/src/components/atoms/item/SbMainItem.tsx
@@ -1,5 +1,6 @@
 import { Divider, Link } from "@chakra-ui/react";
 import { memo, FC } from "react";
+import { useLocation } from "react-router-dom";
 
 type Props = {
   text: string;
@@ -9,9 +10,21 @@ type Props = {
 export const SbMainItem: FC<Props> = memo((props) => {
   const { text, path } = props;
 
+  const location = useLocation();
+  const matchPath = location.pathname === path;
+
   return (
     <>
-      <Link href={path} fontSize="xl" _hover={{ textDecoration: "none" }}>
+      <Link
+        href={path}
+        fontSize="xl"
+        _hover={{ textDecoration: "none" }}
+        bgColor={matchPath ? "orange.100" : ""}
+        borderRadius={matchPath ? "10px" : ""}
+        px="25px"
+        py="2px"
+        mb="4px"
+      >
         {text}
       </Link>
       <Divider width="200px" borderColor="BlackAlpha 800" />
--- profit-system/src/components/atoms/item/SbSubItem.tsx
+++ profit-system/src/components/atoms/item/SbSubItem.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { EditIcon } from "@chakra-ui/icons";
+import { useLocation } from "react-router-dom";
 
 type Props = {
   text: string;
@@ -10,9 +11,19 @@ type Props = {
 export const SbSubItem: FC<Props> = memo((props) => {
   const { text, path } = props;
 
+  const location = useLocation();
+  const matchPath = location.pathname === path;
+
   return (
     <>
-      <Link href={path} mt="10px" _hover={{ textDecoration: "none" }}>
+      <Link
+        href={path}
+        mt="10px"
+        _hover={{ textDecoration: "none" }}
+        bgColor={matchPath ? "blue.200" : ""}
+        borderRadius={matchPath ? "10px" : ""}
+        px="15px"
+      >
         <EditIcon mr="8px" />
         {text}
       </Link>
