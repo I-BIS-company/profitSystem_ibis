ca4af9850bae74a26bf13df239eba6eb87662a61
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-10 15:39:35 +0900

    検索バーの追加

--- profit-system/src/components/pages/ProjectsList.tsx
+++ profit-system/src/components/pages/ProjectsList.tsx
@@ -1,8 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { HeadLine } from "../atoms/HeadLine";
 import { MainScreenTopContainer } from "../molecules/container/MainScreenTopContainer";
-import { DateSearch } from "../atoms/searchs/dateSearch";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { ContentBgTemplate } from "../molecules/container/ContentBgTemplate";
 
@@ -12,7 +17,28 @@ export const ProjectsList: FC = memo(() => {
       <HeadLine Text="案件一覧" />
 
       <MainScreenTopContainer>
-        <DateSearch />
+        <Box display="flex" alignItems="center">
+          <InputGroup>
+            <Input
+              type="text"
+              placeholder="顧客名で検索"
+              paddingRight="0"
+              roundedRight="0"
+            />
+            <InputRightElement width="auto" paddingRight="0">
+              <Button
+                sx={{
+                  borderTopLeftRadius: "0",
+                  borderBottomLeftRadius: "0",
+                }}
+                colorScheme="blue"
+              >
+                検索
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Box>
+
         <Box>
           <PrimaryButton Text="案件を登録する" />
         </Box>
