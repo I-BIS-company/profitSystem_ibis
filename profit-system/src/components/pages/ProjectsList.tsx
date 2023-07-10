09471c989a7354bd79794e391aace1763a952647
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-10 12:44:00 +0900

    コンポーネントテスト 案件一覧画面のレイアウト作成

--- profit-system/src/components/pages/ProjectsList.tsx
+++ profit-system/src/components/pages/ProjectsList.tsx
@@ -1,5 +1,26 @@
+import { Box } from "@chakra-ui/react";
 import { memo, FC } from "react";
+import { HeadLine } from "../atoms/HeadLine";
+import { MainScreenTopContainer } from "../molecules/container/MainScreenTopContainer";
+import { DateSearch } from "../atoms/searchs/dateSearch";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { ContentTemplate } from "../molecules/container/ContentTemplate";
 
 export const ProjectsList: FC = memo(() => {
-  return <p>案件一覧画面です</p>;
+  return (
+    <Box mr="10%" ml="10%" maxW="100%" w="100%" mt="5%">
+      <HeadLine Text="案件一覧" />
+
+      <MainScreenTopContainer>
+        <DateSearch />
+        <Box>
+          <PrimaryButton Text="案件を登録する" />
+        </Box>
+      </MainScreenTopContainer>
+
+      <ContentTemplate>
+        <p>aaa</p>
+      </ContentTemplate>
+    </Box>
+  );
 });
