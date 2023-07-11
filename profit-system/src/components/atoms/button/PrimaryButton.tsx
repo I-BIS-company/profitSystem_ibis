88c49dfa2be66562e877587b33cf23e69a05dda2
Kenji <kkawakami391@gmail.com>
2023-07-11 15:35:24 +0900

    Add size props to buttons.

--- profit-system/src/components/atoms/button/PrimaryButton.tsx
+++ profit-system/src/components/atoms/button/PrimaryButton.tsx
@@ -2,24 +2,24 @@ import { Button } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 type Props = {
-  Text: string;
+  text: string;
+  size: string;
 };
 
 export const PrimaryButton: FC<Props> = memo((props) => {
-    const { Text } = props;
+  const { text, size } = props;
   return (
     <Button
-        bg="green.400"
-        color="white"
-        size="lg"
-        py={2}
-        px={16}
-        shadow="md"
-        variant="outline"
-        _hover={{opacity:.8}}
-        >
-        {Text}
+      bg="green.400"
+      color="white"
+      size={size}
+      py={2}
+      px={16}
+      shadow="md"
+      variant="outline"
+      _hover={{ opacity: 0.8 }}
+    >
+      {text}
     </Button>
-    
   );
 });
