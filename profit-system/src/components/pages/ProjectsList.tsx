5946cd38f3b08d7b23bae49beae05c6cb7d704ce
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-11 11:11:59 +0900

    検索バーをコンポーネント化し、顧客名検索バーでも使用可能に

--- profit-system/src/components/pages/ProjectsList.tsx
+++ profit-system/src/components/pages/ProjectsList.tsx
@@ -1,16 +1,10 @@
-import {
-  Box,
-  Button,
-  Input,
-  InputGroup,
-  InputRightElement,
-} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { HeadLine } from "../atoms/HeadLine";
 import { MainScreenTopContainer } from "../molecules/container/MainScreenTopContainer";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { ContentBgTemplate } from "../molecules/container/ContentBgTemplate";
-import { Search2Icon } from "@chakra-ui/icons";
+import { PrimarySearchButton } from "../atoms/searchs/PrimarySearchButton";
 
 export const ProjectsList: FC = memo(() => {
   return (
@@ -18,32 +12,7 @@ export const ProjectsList: FC = memo(() => {
       <HeadLine text="案件一覧" />
 
       <MainScreenTopContainer>
-        <Box display="flex" alignItems="center">
-          <InputGroup>
-            <Input
-              type="text"
-              placeholder="顧客名で検索"
-              borderRadius="5"
-              bg="white"
-              pr="20"
-              pl="3"
-            />
-            <InputRightElement width="auto" paddingRight="0">
-              <Button
-                sx={{
-                  borderTopLeftRadius: "0",
-                  borderBottomLeftRadius: "0",
-                }}
-                colorScheme="blue"
-                _hover={{ opacity: 0.8 }}
-              >
-                検索
-                <Search2Icon boxSize="5" bg="blue.500" pl="2" />
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </Box>
-
+        <PrimarySearchButton text="案件名" />
         <Box>
           <PrimaryButton text="案件を登録する" />
         </Box>
