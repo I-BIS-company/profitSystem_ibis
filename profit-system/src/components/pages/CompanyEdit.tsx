01fb3585277a7dff5d2c69be48af382d534142fe
Kenji <kkawakami391@gmail.com>
2023-07-11 15:38:26 +0900

    Implement CompanyEditForm component to CompanyEdit page.

--- profit-system/src/components/pages/CompanyEdit.tsx
+++ profit-system/src/components/pages/CompanyEdit.tsx
@@ -1,5 +1,14 @@
 import { memo, FC } from "react";
+import { CompanyFormEdit } from "../organisms/CompanyFormEdit";
+import { Box, Heading } from "@chakra-ui/react";
 
 export const CompanyEdit: FC = memo(() => {
-  return <p>顧客編集画面です</p>;
+  return (
+    <>
+      <Box borderLeft="6px solid blue" borderColor="blue.500" pl="4">
+        <Heading mb="50">顧客編集</Heading>
+      </Box>
+      <CompanyFormEdit />
+    </>
+  );
 });
