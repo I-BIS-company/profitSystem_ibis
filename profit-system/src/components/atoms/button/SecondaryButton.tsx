5b49a84699bbcdcbcab7e70ee4d4122fbab84ccc
Kenji <kkawakami391@gmail.com>
2023-07-11 09:43:01 +0900

    Add size props to Primary / Secondary Button components.

--- profit-system/src/components/atoms/button/SecondaryButton.tsx
+++ profit-system/src/components/atoms/button/SecondaryButton.tsx
@@ -2,25 +2,25 @@ import { Button } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 type Props = {
-  Text: string;
+  text: string;
+  size: string;
 };
 
 export const SecondaryButton: FC<Props> = memo((props) => {
-    const { Text } = props;
+  const { text, size } = props;
   return (
     <Button
-        bg="white"
-        color="blackAlpha.900"
-        size="md"
-        py={2}
-        px={6}
-        shadow="md"
-        variant="outline"
-        borderColor="black"
-        _hover={{opacity:.8}}
-        >
-        {Text}
+      bg="white"
+      color="blackAlpha.900"
+      size={size}
+      py={2}
+      px={6}
+      shadow="md"
+      variant="outline"
+      borderColor="black"
+      _hover={{ opacity: 0.8 }}
+    >
+      {text}
     </Button>
-    
   );
 });
