207bc72433229b962a0d5e05484edd57f5b516d6
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-11 11:43:51 +0900

    不要なフラグメントの削除

--- profit-system/src/components/molecules/tablelist/LogTableTemplate.tsx
+++ profit-system/src/components/molecules/tablelist/LogTableTemplate.tsx
@@ -6,49 +6,47 @@ import { SecondaryButton } from "../../atoms/button/SecondaryButton";
 
 export const LogTableTemplate: FC = memo(() => {
   return (
-    <>
-      <TableContainer>
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <TableHead text="担当者名" />
-              <TableHead text="担当案件" />
-              <TableHead text="時間(工数)" />
-            </Tr>
-          </Thead>
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <TableHead text="担当者名" />
+            <TableHead text="担当案件" />
+            <TableHead text="時間(工数)" />
+          </Tr>
+        </Thead>
 
-          <Tbody alignItems="center">
-            <Tr fontSize="14">
-              <TableBody text="ユーザ 太郎" />
-              <TableBody text="xxxxシステム開発" />
-              <TableBody text="10時間" />
+        <Tbody alignItems="center">
+          <Tr fontSize="14">
+            <TableBody text="ユーザ 太郎" />
+            <TableBody text="xxxxシステム開発" />
+            <TableBody text="10時間" />
 
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </Tr>
-            <Tr fontSize="14">
-              <TableBody text="ユーザ 太郎" />
-              <TableBody text="xxxxシステム開発" />
-              <TableBody text="10時間" />
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              pt="2.5"
+            >
+              <SecondaryButton text="編集" size="sm" />
+            </Box>
+          </Tr>
+          <Tr fontSize="14">
+            <TableBody text="ユーザ 太郎" />
+            <TableBody text="xxxxシステム開発" />
+            <TableBody text="10時間" />
 
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              pt="2.5"
+            >
+              <SecondaryButton text="編集" size="sm" />
+            </Box>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 });
