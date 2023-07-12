2636bdaefe56f669495094bb24798347fa312f07
Kenji <kkawakami391@gmail.com>
2023-07-12 13:38:57 +0900

    Create PostIcon and replace previous icon.

--- profit-system/src/components/atoms/item/PostNumFormItem.tsx
+++ profit-system/src/components/atoms/item/PostNumFormItem.tsx
@@ -1,7 +1,12 @@
-import { Flex, FormLabel, Icon, Input, Text } from "@chakra-ui/react";
-import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Flex, FormLabel, Input, Text } from "@chakra-ui/react";
+import { createIcon } from "@chakra-ui/react";
 import { memo, FC } from "react";
+
+const PostIcon = createIcon({
+  displayName: "PostIcon",
+  viewBox: "0 0 512 512",
+  d: "M2.823 0H509.17699999999996V108.506H2.823zM2.823 307.432L201.75 307.432 201.75 512 310.25 512 310.25 307.432 509.177 307.432 509.177 198.927 2.823 198.927z",
+});
 
 export const PostNumFormItem: FC = memo(() => {
   return (
@@ -10,7 +15,7 @@ export const PostNumFormItem: FC = memo(() => {
         郵便番号
       </FormLabel>
       <Flex alignItems="center">
-        <Icon as={FontAwesomeIcon} icon={faSquareEnvelope} boxSize={6} />
+        <PostIcon color="blackAlpha.500" />
         <Input mx="18px" w="150px" />
         <Text fontSize="2xl">-</Text>
         <Input mx="18px" w="150px" />
