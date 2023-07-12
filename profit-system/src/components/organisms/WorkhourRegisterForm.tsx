358537f66d7cea7851e7a3b7c773cd940b18d279
Kenji <kkawakami391@gmail.com>
2023-07-12 18:48:16 +0900

    Add button container and implement.

--- profit-system/src/components/organisms/WorkhourRegisterForm.tsx
+++ profit-system/src/components/organisms/WorkhourRegisterForm.tsx
@@ -1,4 +1,4 @@
-import { Flex, FormControl } from "@chakra-ui/react";
+import { FormControl } from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
@@ -6,6 +6,7 @@ import { MainContentContainer } from "../molecules/container/MainContentContaine
 import { WorkhourFormContainer } from "../molecules/container/WorkhourContainer";
 import { ProjectFormSelect } from "../molecules/select/ProjectFormSelect";
 import { WorkhourFormSelect } from "../molecules/select/WorkhourFormSelect";
+import { FormButtonContainer } from "../molecules/container/FormButtonContainer";
 
 export const WorkhourRegisterForm: FC = memo(() => {
   return (
@@ -14,10 +15,10 @@ export const WorkhourRegisterForm: FC = memo(() => {
         <WorkhourFormContainer>
           <ProjectFormSelect />
           <WorkhourFormSelect />
-          <Flex w="45%" justifyContent="center" alignItems="center" gap="50px">
+          <FormButtonContainer>
             <SecondaryButton text="一覧に戻る" size="md" px="16" />
             <PrimaryButton text="登録" size="md" />
-          </Flex>
+          </FormButtonContainer>
         </WorkhourFormContainer>
       </FormControl>
     </MainContentContainer>
