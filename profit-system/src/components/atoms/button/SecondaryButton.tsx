00ea79ff5f84fb9021456c4cdda11ee3d367304a
Kenji <kkawakami391@gmail.com>
2023-07-12 09:52:38 +0900

    Fix props and add size, px props to SecondaryButton.

--- profit-system/src/components/atoms/button/SecondaryButton.tsx
+++ profit-system/src/components/atoms/button/SecondaryButton.tsx
@@ -2,25 +2,26 @@ import { Button } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 type Props = {
-  Text: string;
+  text: string;
+  size: string;
+  px: string;
 };
 
 export const SecondaryButton: FC<Props> = memo((props) => {
-    const { Text } = props;
+  const { text, size, px } = props;
   return (
     <Button
-        bg="white"
-        color="blackAlpha.900"
-        size="md"
-        py={2}
-        px={6}
-        shadow="md"
-        variant="outline"
-        borderColor="black"
-        _hover={{opacity:.8}}
-        >
-        {Text}
+      bg="white"
+      color="blackAlpha.900"
+      size={size}
+      py={2}
+      px={px}
+      shadow="md"
+      variant="outline"
+      borderColor="black"
+      _hover={{ opacity: 0.8 }}
+    >
+      {text}
     </Button>
-    
   );
 });
