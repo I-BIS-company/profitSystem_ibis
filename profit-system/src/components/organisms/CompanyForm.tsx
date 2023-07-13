26ff48e3d5c7cec33c9f85a625575fc03721b1e7
Kenji <kkawakami391@gmail.com>
2023-07-13 14:10:43 +0900

    Review's fix

--- profit-system/src/components/organisms/CompanyForm.tsx
+++ profit-system/src/components/organisms/CompanyForm.tsx
@@ -4,8 +4,6 @@ import { PrimaryFormItem } from "../molecules/item/PrimaryFormItem";
 import { PostNumFormItem } from "../molecules/item/PostNumFormItem";
 import { PhoneFormItem } from "../molecules/item/PhoneFormItem";
 import { FormButtonContainer } from "../molecules/container/FormButtonContainer";
-import { SecondaryButton } from "../atoms/button/SecondaryButton";
-import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { MainContentContainer } from "../molecules/container/MainContentContainer";
 
 export const CompanyForm: FC = memo(() => {
@@ -16,10 +14,7 @@ export const CompanyForm: FC = memo(() => {
         <PostNumFormItem />
         <PrimaryFormItem text="住所" />
         <PhoneFormItem />
-        <FormButtonContainer>
-          <SecondaryButton text="一覧に戻る" size="md" />
-          <PrimaryButton text="登録" size="md" />
-        </FormButtonContainer>
+        <FormButtonContainer />
       </MainContentContainer>
     </FormControl>
   );
