44f2cae6253ccbe00f834057631fbc3e9535252b
Kenji <kkawakami391@gmail.com>
2023-07-13 14:52:00 +0900

    Post conflict resolve fix.

--- profit-system/src/components/organisms/CompanyFormEdit.tsx
+++ profit-system/src/components/organisms/CompanyFormEdit.tsx
@@ -1,46 +1,20 @@
 import { FormControl } from "@chakra-ui/react";
 import { FC, memo } from "react";
-import { FormItemContainer } from "../molecules/container/FormItemContainer";
-import { PrimaryFormItem } from "../atoms/item/PrimaryFormItem";
-import { PostNumFormItem } from "../atoms/item/PostNumFormItem";
-import { PhoneFormItem } from "../atoms/item/PhoneFormItem";
 import { FormButtonContainer } from "../molecules/container/FormButtonContainer";
-import { SecondaryButton } from "../atoms/button/SecondaryButton";
-import { PrimaryButton } from "../atoms/button/PrimaryButton";
-import { FormDivider } from "../atoms/FormDivider";
 import { MainContentContainer } from "../molecules/container/MainContentContainer";
+import { PrimaryFormItem } from "../molecules/item/PrimaryFormItem";
+import { PostNumFormItem } from "../molecules/item/PostNumFormItem";
+import { PhoneFormItem } from "../molecules/item/PhoneFormItem";
 
 export const CompanyFormEdit: FC = memo(() => {
   return (
     <FormControl>
       <MainContentContainer>
-        <FormItemContainer>
-          <PrimaryFormItem text="顧客名" placeholder="株式会社A" />
-        </FormItemContainer>
-        <FormDivider />
-        <FormItemContainer>
-          <PostNumFormItem placeholder1="611" placeholder2="0000" />
-        </FormItemContainer>
-        <FormDivider />
-        <FormItemContainer>
-          <PrimaryFormItem
-            text="住所"
-            placeholder="東京都葛飾立石町3丁目23-55"
-          />
-        </FormItemContainer>
-        <FormDivider />
-        <FormItemContainer>
-          <PhoneFormItem
-            placeholder1="010"
-            placeholder2="2222"
-            placeholder3="3333"
-          />
-        </FormItemContainer>
-        <FormDivider />
-        <FormButtonContainer>
-          <SecondaryButton text="一覧に戻る" size="md" />
-          <PrimaryButton text="登録" size="md" />
-        </FormButtonContainer>
+        <PrimaryFormItem text="顧客名" />
+        <PostNumFormItem />
+        <PrimaryFormItem text="住所" />
+        <PhoneFormItem />
+        <FormButtonContainer />
       </MainContentContainer>
     </FormControl>
   );
