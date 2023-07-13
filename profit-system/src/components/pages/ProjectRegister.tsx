b0083862fa9d353d3b2aef748809d61b88c81c66
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-13 11:37:17 +0900

    レビューの修正

--- profit-system/src/components/pages/ProjectRegister.tsx
+++ profit-system/src/components/pages/ProjectRegister.tsx
@@ -1,14 +1,11 @@
-import { Box, Heading } from "@chakra-ui/layout";
 import { memo, FC } from "react";
 import { ProjectForm } from "../organisms/ProjectForm";
-// import { CompanyForm } from "../organisms/CompanyForm";
+import { HeadLine } from "../atoms/HeadLine";
 
 export const ProjectRegister: FC = memo(() => {
   return (
     <>
-      <Box borderLeft="6px solid blue" borderColor="blue.500" pl="4">
-        <Heading mb="50">案件登録</Heading>
-      </Box>
+      <HeadLine text="案件登録" />
       <ProjectForm />
     </>
   );
