697751872f2e7fe0f9e0dc7790e7b04778d1f4c7
Kenji <kkawakami391@gmail.com>
2023-07-13 15:56:51 +0900

    Fix ci check problem and add px props to FormButtonContainer for better button layout.

--- profit-system/src/components/molecules/container/FormButtonContainer.tsx
+++ profit-system/src/components/molecules/container/FormButtonContainer.tsx
@@ -3,10 +3,15 @@ import { memo, FC } from "react";
 import { SecondaryButton } from "../../atoms/button/SecondaryButton";
 import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
-export const FormButtonContainer: FC = memo(() => {
+type Props = {
+  secondaryPx?: string;
+};
+
+export const FormButtonContainer: FC<Props> = memo((props) => {
+  const { secondaryPx } = props;
   return (
     <Flex w="45%" justifyContent="center" alignItems="center" gap="50px">
-      <SecondaryButton text="一覧に戻る" />
+      <SecondaryButton text="一覧に戻る" px={secondaryPx} />
       <PrimaryButton text="登録" />
     </Flex>
   );
