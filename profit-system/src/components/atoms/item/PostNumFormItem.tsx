bdfcc9129c74defe59f82ec7cab770ba4c6b5dee
Kenji <kkawakami391@gmail.com>
2023-07-13 10:06:45 +0900

    Fix Post Icon.

--- profit-system/src/components/atoms/item/PostNumFormItem.tsx
+++ profit-system/src/components/atoms/item/PostNumFormItem.tsx
@@ -1,7 +1,11 @@
-import { Flex, FormLabel, Icon, Input, Text } from "@chakra-ui/react";
-import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Flex, FormLabel, Input, Text, createIcon } from "@chakra-ui/react";
 import { memo, FC } from "react";
+
+const PostIcon = createIcon({
+  displayName: "PostIcon",
+  viewBox: "0 0 512 512",
+  d: "M2.823 0H509.17699999999996V108.506H2.823zM2.823 307.432L201.75 307.432 201.75 512 310.25 512 310.25 307.432 509.177 307.432 509.177 198.927 2.823 198.927z",
+});
 
 type Props = {
   placeholder1?: string;
@@ -16,7 +20,7 @@ export const PostNumFormItem: FC<Props> = memo((props) => {
         郵便番号
       </FormLabel>
       <Flex alignItems="center">
-        <Icon as={FontAwesomeIcon} icon={faSquareEnvelope} boxSize={6} />
+        <PostIcon color="blackAlpha.500" />
         <Input mx="18px" w="150px" placeholder={placeholder1} />
         <Text fontSize="2xl">-</Text>
         <Input mx="18px" w="150px" placeholder={placeholder2} />
