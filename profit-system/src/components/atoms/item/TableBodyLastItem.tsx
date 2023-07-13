5305116455a4708aaf7788f5d011f102fd4a05da
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-13 14:24:34 +0900

    不要になった三項演算子の部分を削除

--- profit-system/src/components/atoms/item/TableBodyLastItem.tsx
+++ profit-system/src/components/atoms/item/TableBodyLastItem.tsx
@@ -3,19 +3,16 @@ import { FC, ReactNode } from "react";
 
 type Props = {
   text: string;
-  noBorder?: boolean;
   children?: ReactNode;
 };
 
 export const TableBodyLastItem: FC<Props> = (props) => {
-  const { text, noBorder, children } = props;
-  const borderRightColor = noBorder ? "transparent" : "blackAlpha.200";
+  const { text, children } = props;
 
   return (
     <Td
       borderRight="1px solid"
       border="none"
-      borderRightColor={borderRightColor}
       display="flex"
       justifyContent="space-between"
       alignItems="center"
