df68551b1b97366f071645e02d2c7b7e28307987
Kenji <kkawakami391@gmail.com>
2023-07-14 17:17:10 +0900

    Resolve CI Check problems.

--- profit-system/src/components/pages/CompanyRegister.tsx
+++ profit-system/src/components/pages/CompanyRegister.tsx
@@ -1,12 +1,12 @@
 import { memo, FC } from "react";
-import { CompanyForm } from "../organisms/CompanyForm";
+import { CompanyRegisterForm } from "../organisms/form/CompanyRegisterForm";
 import { HeadLine } from "../atoms/HeadLine";
 
 export const CompanyRegister: FC = memo(() => {
   return (
     <>
       <HeadLine text="顧客登録" />
-      <CompanyForm />
+      <CompanyRegisterForm />
     </>
   );
 });
