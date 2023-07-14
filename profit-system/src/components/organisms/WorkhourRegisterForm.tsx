82adceb90663b4950ddba2355a235d3fc5885e84
Kenji <kkawakami391@gmail.com>
2023-07-14 13:50:58 +0900

    Fix component file name.

--- profit-system/src/components/organisms/WorkhourRegisterForm.tsx
+++ profit-system/src/components/organisms/WorkhourRegisterForm.tsx
@@ -1,7 +1,7 @@
 import { FormControl } from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { MainContentContainer } from "../molecules/container/MainContentContainer";
-import { WorkhourFormContainer } from "../molecules/container/WorkhourContainer";
+import { WorkhourFormContainer } from "../molecules/container/WorkhourFormContainer";
 import { ProjectFormSelect } from "../molecules/select/ProjectFormSelect";
 import { WorkhourFormSelect } from "../molecules/select/WorkhourFormSelect";
 import { FormButtonContainer } from "../molecules/container/FormButtonContainer";
