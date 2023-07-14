5e15fa9a6d332b874ce9c0bc3452734e66518fde
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-14 17:23:52 +0900

    map関数を使用してコードを簡略化

--- profit-system/src/components/molecules/list/UsersTableTemplateList.tsx
+++ profit-system/src/components/molecules/list/UsersTableTemplateList.tsx
@@ -1,11 +1,18 @@
-import { Box, Table, TableContainer, Tbody, Thead, Tr } from "@chakra-ui/react";
+import { Table, TableContainer, Tbody, Thead, Tr } from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { TableHeadItem } from "../../atoms/item/TableHeadItem";
 import { TableBodyItem } from "../../atoms/item/TableBodyItem";
-import { SecondaryButton } from "../../atoms/button/SecondaryButton";
-import { EditItem } from "../../atoms/item/EditItem";
+import { EditItem } from "../EditItem";
 
 export const UsersTableTemplateList: FC = memo(() => {
+  const userData = [
+    { userName: "ユーザ 太郎", isAdmin: "投稿者", cost: "1,000円" },
+    { userName: "ユーザ 二郎", isAdmin: "投稿者", cost: "1,200円" },
+    { userName: "ユーザ 花子", isAdmin: "管理者", cost: "1,000円" },
+    { userName: "ユーザ 二郎", isAdmin: "投稿者", cost: "1,500円" },
+    { userName: "ユーザ 太郎", isAdmin: "投稿者", cost: "2,000円" },
+  ];
+
   return (
     <TableContainer>
       <Table variant="simple" border="none">
@@ -14,85 +21,18 @@ export const UsersTableTemplateList: FC = memo(() => {
             <TableHeadItem text="ユーザ名" />
             <TableHeadItem text="権限" />
             <TableHeadItem text="作業コスト" />
+            <TableHeadItem />
           </Tr>
         </Thead>
-
         <Tbody alignItems="center">
-          <Tr fontSize="14">
-            <TableBodyItem text="ユーザ 太郎" />
-            <TableBodyItem text="投稿者" />
-            <TableBodyItem text="1,000円" />
-            <EditItem>
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </EditItem>
-          </Tr>
-          <Tr fontSize="14">
-            <TableBodyItem text="ユーザ 二郎" />
-            <TableBodyItem text="投稿者" />
-            <TableBodyItem text="1,200円" />
-            <EditItem>
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </EditItem>
-          </Tr>
-          <Tr fontSize="14">
-            <TableBodyItem text="ユーザ 花子" />
-            <TableBodyItem text="管理者" />
-            <TableBodyItem text="1,000円" />
-            <EditItem>
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </EditItem>
-          </Tr>
-          <Tr fontSize="14">
-            <TableBodyItem text="ユーザ 二郎" />
-            <TableBodyItem text="投稿者" />
-            <TableBodyItem text="1,500円" />
-            <EditItem>
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </EditItem>
-          </Tr>
-          <Tr fontSize="14">
-            <TableBodyItem text="ユーザ 太郎" />
-            <TableBodyItem text="投稿者" />
-            <TableBodyItem text="2,000円" />
-            <EditItem>
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                pt="2.5"
-              >
-                <SecondaryButton text="編集" size="sm" />
-              </Box>
-            </EditItem>
-          </Tr>
+          {userData.map((user) => (
+            <Tr key={user.userName} fontSize="14">
+              <TableBodyItem text={user.userName} />
+              <TableBodyItem text={user.isAdmin} />
+              <TableBodyItem text={user.cost} />
+              <EditItem />
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </TableContainer>
