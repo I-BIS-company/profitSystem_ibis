81878a8ff8ca2053f11eee2dc7d03fba6fa5710e
Kenji <kkawakami391@gmail.com>
2023-07-16 14:46:32 +0900

    Add default value to input.

--- profit-system/src/components/organisms/form/CompanyEditForm.tsx
+++ profit-system/src/components/organisms/form/CompanyEditForm.tsx
@@ -10,10 +10,10 @@ export const CompanyFormEdit: FC = memo(() => {
   return (
     <FormControl>
       <MainContentContainer>
-        <PrimaryFormItem text="顧客名" />
-        <PostNumFormItem />
-        <PrimaryFormItem text="住所" />
-        <PhoneFormItem />
+        <PrimaryFormItem text="顧客名" value="株式会社A" />
+        <PostNumFormItem value1="611" value2="0000" />
+        <PrimaryFormItem text="住所" value="東京都葛飾区立石町3丁目 23-55" />
+        <PhoneFormItem value1="030" value2="0000" value3="3333" />
         <FormButtonContainer />
       </MainContentContainer>
     </FormControl>
