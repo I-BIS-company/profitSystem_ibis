8b316bc0c3fb2c41f50fa3ce86a6b6a1af466607
Kenji <kkawakami391@gmail.com>
2023-07-17 00:28:08 +0900

    Add PrimaryButton text props to ButtonContainer, and implementation.

--- profit-system/src/components/molecules/container/FormButtonContainer.tsx
+++ profit-system/src/components/molecules/container/FormButtonContainer.tsx
@@ -6,20 +6,32 @@ import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 type Props = {
   secondaryPx?: string;
   ml?: string;
+  gap?: string;
+  h?: string;
+  w?: string;
+  primaryText?: string;
 };
 
 export const FormButtonContainer: FC<Props> = memo((props) => {
-  const { secondaryPx, ml } = props;
+  const {
+    secondaryPx,
+    ml,
+    gap = "50px",
+    h,
+    w = "45%",
+    primaryText = "登録",
+  } = props;
   return (
     <Flex
-      w="45%"
+      w={w}
+      h={h}
       justifyContent="center"
       alignItems="center"
-      gap="50px"
+      gap={gap}
       ml={ml}
     >
       <SecondaryButton text="一覧に戻る" px={secondaryPx} />
-      <PrimaryButton text="登録" />
+      <PrimaryButton text={primaryText} />
     </Flex>
   );
 });
