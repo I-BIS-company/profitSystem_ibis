e2a8d7b7f4d09f1a4fb564ceea92e110be32ccd3
Kenji <kkawakami391@gmail.com>
2023-07-17 11:23:23 +0900

    Remove h and gap props from ButtonContainer.

--- profit-system/src/components/molecules/container/FormButtonContainer.tsx
+++ profit-system/src/components/molecules/container/FormButtonContainer.tsx
@@ -6,30 +6,14 @@ import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 type Props = {
   secondaryPx?: string;
   ml?: string;
-  gap?: string;
-  h?: string;
   w?: string;
   primaryText?: string;
 };
 
 export const FormButtonContainer: FC<Props> = memo((props) => {
-  const {
-    secondaryPx,
-    ml,
-    gap = "50px",
-    h,
-    w = "45%",
-    primaryText = "登録",
-  } = props;
+  const { secondaryPx, ml, w = "45%", primaryText = "登録" } = props;
   return (
-    <Flex
-      w={w}
-      h={h}
-      justifyContent="center"
-      alignItems="center"
-      gap={gap}
-      ml={ml}
-    >
+    <Flex w={w} justifyContent="center" alignItems="center" gap="50px" ml={ml}>
       <SecondaryButton text="一覧に戻る" px={secondaryPx} />
       <PrimaryButton text={primaryText} />
     </Flex>
