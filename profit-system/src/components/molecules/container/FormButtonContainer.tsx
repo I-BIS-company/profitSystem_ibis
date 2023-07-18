9be637dab2581da5deb253d596c385c7450b6243
Kenji <kkawakami391@gmail.com>
2023-07-18 11:49:04 +0900

    Remove unnecessary props from FormButtonContainer.

--- profit-system/src/components/molecules/container/FormButtonContainer.tsx
+++ profit-system/src/components/molecules/container/FormButtonContainer.tsx
@@ -6,30 +6,13 @@ import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 type Props = {
   secondaryPx?: string;
   ml?: string;
-  gap?: string;
-  h?: string;
-  w?: string;
   primaryText?: string;
 };
 
 export const FormButtonContainer: FC<Props> = memo((props) => {
-  const {
-    secondaryPx,
-    ml,
-    gap = "50px",
-    h,
-    w = "45%",
-    primaryText = "登録",
-  } = props;
+  const { secondaryPx, ml, primaryText = "登録" } = props;
   return (
-    <Flex
-      w={w}
-      h={h}
-      justifyContent="center"
-      alignItems="center"
-      gap={gap}
-      ml={ml}
-    >
+    <Flex justifyContent="center" alignItems="center" gap="50px" ml={ml}>
       <SecondaryButton text="一覧に戻る" px={secondaryPx} />
       <PrimaryButton text={primaryText} />
     </Flex>
