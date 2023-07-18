df78f30f7a24630b1912b436c92f74d0626279f9
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-18 14:19:17 +0900

    レイアウトの修正

--- profit-system/src/components/pages/ProfitsList.tsx
+++ profit-system/src/components/pages/ProfitsList.tsx
@@ -12,42 +12,56 @@ export const ProfitsList: FC = memo(() => {
       <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
         <ProfitCardContainer>
           <ProfitHeadlineItem text="A" />
-          <Box textAlign="left" mx="auto">
-            {/* <Flex justifyContent="space-between" mx="50px"> */}
-            <Flex flexShrink="1" justifyContent="space-between" mx="50px">
-              <ProfitInfoItem pTitle="売上" pDescription="1,000,000" />
-              <ProfitInfoItem pTitle="粗利益" pDescription="500,000" />
+          <Box>
+            <Flex mx="50px">
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem pTitle="売上" pDescription="1,000,000" />
+              </Box>
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem pTitle="粗利益" pDescription="500,000" />
+              </Box>
             </Flex>
             <Divider my="4" />
 
-            <Flex flexShrink="1" justifyContent="space-between" mx="50px">
-              {/* <Flex justifyContent="space-between" mx="50px"> */}
-              <ProfitInfoItem pTitle="コスト合計" pDescription="500,000" />
-              <ProfitInfoItem
-                pTitle="稼働時間"
-                pDescription="200"
-                // unit="時間"
-              />
+            <Flex mx="50px">
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem pTitle="コスト合計" pDescription="500,000" />
+              </Box>
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem
+                  pTitle="稼働時間"
+                  pDescription="200"
+                  // unit="時間"
+                />
+              </Box>
             </Flex>
           </Box>
         </ProfitCardContainer>
 
         <ProfitCardContainer>
           <ProfitHeadlineItem text="B" />
-          <Box textAlign="left" mx="auto">
-            <Flex flexShrink="1" justifyContent="space-between" mx="50px">
-              <ProfitInfoItem pTitle="売上" pDescription="2,000,000" />
-              <ProfitInfoItem pTitle="粗利益" pDescription="1,000,000" />
+          <Box>
+            <Flex mx="50px">
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem pTitle="売上" pDescription="2,000,000" />
+              </Box>
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem pTitle="粗利益" pDescription="1,000,000" />
+              </Box>
             </Flex>
             <Divider my="4" />
 
-            <Flex flexShrink="1" justifyContent="space-between" mx="50px">
-              <ProfitInfoItem pTitle="コスト合計" pDescription="1,000,000" />
-              <ProfitInfoItem
-                pTitle="稼働時間"
-                pDescription="200"
-                // unit="時間"
-              />
+            <Flex mx="50px">
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem pTitle="コスト合計" pDescription="1,000,000" />
+              </Box>
+              <Box w="50%" flexShrink="1">
+                <ProfitInfoItem
+                  pTitle="稼働時間"
+                  pDescription="200"
+                  // unit="時間"
+                />
+              </Box>
             </Flex>
           </Box>
         </ProfitCardContainer>
