4bf757be49168684cda86e7c5de276f7804d6c38
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-18 12:23:36 +0900

    一覧画面にiconbuttonコンポーネントをインポート

--- profit-system/src/components/pages/ProjectsList.tsx
+++ profit-system/src/components/pages/ProjectsList.tsx
@@ -2,10 +2,10 @@ import { Box } from "@chakra-ui/react";
 import { memo, FC } from "react";
 import { HeadLine } from "../atoms/HeadLine";
 import { MainScreenTopContainer } from "../molecules/container/MainScreenTopContainer";
-import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { ContentBgTemplate } from "../molecules/container/ContentBgTemplateContainer";
 import { PrimarySearchButton } from "../atoms/button/PrimarySearchButton";
 import { ProjectTableTemplateList } from "../molecules/list/ProjectTableTemplateList";
+import { IconButton } from "../atoms/button/IconButton";
 
 export const ProjectsList: FC = memo(() => {
   return (
@@ -14,7 +14,7 @@ export const ProjectsList: FC = memo(() => {
       <MainScreenTopContainer>
         <PrimarySearchButton text="案件名" />
         <Box>
-          <PrimaryButton text="案件を登録する" size="lg" showIcon={true} />
+          <IconButton text="案件を登録する" />
         </Box>
       </MainScreenTopContainer>
       <ContentBgTemplate>
