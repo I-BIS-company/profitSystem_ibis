5f8dba8dd7dd4f55351377954bcad8c523e3b4e5
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-19 13:33:20 +0900

    ログインボタン押下時、Authenticationにデータ追加

--- profit-system/src/components/atoms/button/LoginButton.tsx
+++ profit-system/src/components/atoms/button/LoginButton.tsx
@@ -1,19 +1,26 @@
 import { Button } from "@chakra-ui/react";
-import { FC } from "react";
+import { signInWithPopup } from "@firebase/auth";
+import { FC, memo } from "react";
+import { auth, provider } from "../../../firebase";
 
-export const LoginButton:FC= () => {
-    return(
-        <Button
-          bg="orange.300"
-          color="blackAlpha.900"
-          size="lg"
-          py={8}
-          px={24}
-          shadow="md"
-          _hover={{opacity:.8}}
-          >
-          ログイン
-        </Button>
-    )
-
-};
+export const LoginButton: FC = memo(() => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
+  return (
+    <Button
+      onClick={signIn}
+      bg="orange.300"
+      color="blackAlpha.900"
+      size="lg"
+      py={8}
+      px={24}
+      shadow="md"
+      _hover={{ opacity: 0.8 }}
+    >
+      ログイン
+    </Button>
+  );
+});
