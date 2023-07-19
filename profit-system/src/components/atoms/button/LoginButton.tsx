ce2cad56b554242feb48e91ac826a62706a7df04
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-19 15:52:00 +0900

    test

--- profit-system/src/components/atoms/button/LoginButton.tsx
+++ profit-system/src/components/atoms/button/LoginButton.tsx
@@ -1,17 +1,55 @@
-import { Button } from "@chakra-ui/react";
-import { signInWithPopup } from "@firebase/auth";
-import { FC, memo } from "react";
-import { auth, provider } from "../../../firebase";
+// import { Button } from "@chakra-ui/react";
+// import { signInWithPopup } from "@firebase/auth";
+// import { FC, memo } from "react";
+// import { auth, provider } from "../../../firebase";
 
-export const LoginButton: FC = memo(() => {
-  const signIn = () => {
-    signInWithPopup(auth, provider).catch((err) => {
-      alert(err.message);
-    });
+// export const LoginButton: FC = memo(() => {
+//   const signIn = () => {
+//     signInWithPopup(auth, provider).catch((err) => {
+//       alert(err.message);
+//     });
+//   };
+//   return (
+//     <Button
+//       onClick={signIn}
+//       bg="orange.300"
+//       color="blackAlpha.900"
+//       size="lg"
+//       py={8}
+//       px={24}
+//       shadow="md"
+//       _hover={{ opacity: 0.8 }}
+//     >
+//       ログイン
+//     </Button>
+//   );
+// });
+
+import { Button } from "@chakra-ui/react";
+import { useState } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../../firebase";
+import { useHistory } from "react-router-dom";
+
+export const LoginButton = () => {
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      await signInWithPopup(auth, provider);
+      history.push("/log");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <Button
-      onClick={signIn}
+      onClick={handleLogin}
       bg="orange.300"
       color="blackAlpha.900"
       size="lg"
@@ -19,8 +57,10 @@ export const LoginButton: FC = memo(() => {
       px={24}
       shadow="md"
       _hover={{ opacity: 0.8 }}
+      isLoading={loading}
+      loadingText="ログイン中..."
     >
       ログイン
     </Button>
   );
-});
+};
