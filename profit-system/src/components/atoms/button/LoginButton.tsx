ae217e3bc29955a71d444043b5b9c4097d5f2c98
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-20 01:09:22 +0900

    usersテーブルにログインデータを追加

--- profit-system/src/components/atoms/button/LoginButton.tsx
+++ profit-system/src/components/atoms/button/LoginButton.tsx
@@ -3,16 +3,35 @@ import { signInWithPopup } from "@firebase/auth";
 import { FC, memo } from "react";
 import { auth, provider } from "../../../firebase";
 import { useNavigate } from "react-router-dom";
+import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
 
 export const LoginButton: FC = memo(() => {
   const navigate = useNavigate();
+  const Firestore = getFirestore();
 
   const signIn = async () => {
     try {
       await signInWithPopup(auth, provider);
+
+      // 初回ログイン時のユーザ情報をFirestoreに保存する
+      if (auth.currentUser) {
+        const { uid, displayName } = auth.currentUser;
+
+        // Firestoreへの参照を作成
+        const usersCollectionRef = collection(Firestore, "users");
+
+        // ユーザ情報を登録するドキュメントの参照を作成
+        const userDocRef = doc(usersCollectionRef, uid);
+
+        // ユーザ情報を登録
+        await setDoc(userDocRef, {
+          id: uid,
+          name: displayName,
+        });
+      }
       navigate("/log");
     } catch (error: any) {
-      console.log(error.message);
+      alert(error.message);
     }
   };
 
