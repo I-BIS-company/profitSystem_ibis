dcf2128bd727df9ec475e285d485dac033520bd3
Kenji <kkawakami391@gmail.com>
2023-07-19 15:02:40 +0900

    Add click event to register on DB.

--- profit-system/src/components/molecules/container/FormButtonContainer.tsx
+++ profit-system/src/components/molecules/container/FormButtonContainer.tsx
@@ -7,14 +7,15 @@ type Props = {
   secondaryPx?: string;
   ml?: string;
   primaryText?: string;
+  onClick: () => void;
 };
 
 export const FormButtonContainer: FC<Props> = memo((props) => {
-  const { secondaryPx, ml, primaryText = "登録" } = props;
+  const { secondaryPx, ml, primaryText = "登録", onClick } = props;
   return (
     <Flex justifyContent="center" alignItems="center" gap="50px" ml={ml}>
       <SecondaryButton text="一覧に戻る" px={secondaryPx} />
-      <PrimaryButton text={primaryText} />
+      <PrimaryButton text={primaryText} onClick={onClick} />
     </Flex>
   );
 });
