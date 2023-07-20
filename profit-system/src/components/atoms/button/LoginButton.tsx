b1f63b0dd66bff24967ddda6d03d4c0413a290ab
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-20 16:37:49 +0900

    2回目の登録をスキップする機能追加

--- profit-system/src/components/atoms/button/LoginButton.tsx
+++ profit-system/src/components/atoms/button/LoginButton.tsx
@@ -1,33 +1,51 @@
 import { Button } from "@chakra-ui/react";
-import { signInWithPopup } from "@firebase/auth";
-import { FC, memo } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { FC, memo, useEffect, useState } from "react";
 import { auth, provider } from "../../../firebase";
 import { useNavigate } from "react-router-dom";
-import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getFirestore,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
 
 export const LoginButton: FC = memo(() => {
   const navigate = useNavigate();
-  const Firestore = getFirestore();
+  const firestore = getFirestore();
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  useEffect(() => {
+    checkRegistration();
+  }, []);
+
+  const checkRegistration = async () => {
+    if (auth.currentUser) {
+      const { uid } = auth.currentUser;
+      const userDocRef = doc(collection(firestore, "users"), uid);
+      const docSnapshot = await getDoc(userDocRef);
+      setIsRegistered(
+        docSnapshot.exists() && docSnapshot.data().registered === true
+      );
+    }
+  };
 
   const signIn = async () => {
     try {
       await signInWithPopup(auth, provider);
 
-      // 初回ログイン時のユーザ情報をFirestoreに保存する
       if (auth.currentUser) {
         const { uid, displayName } = auth.currentUser;
 
-        // Firestoreへの参照を作成
-        const usersCollectionRef = collection(Firestore, "users");
-
-        // ユーザ情報を登録するドキュメントの参照を作成
-        const userDocRef = doc(usersCollectionRef, uid);
-
-        // ユーザ情報を登録
-        await setDoc(userDocRef, {
-          id: uid,
-          name: displayName,
-        });
+        if (!isRegistered) {
+          const userDocRef = doc(collection(firestore, "users"), uid);
+          await setDoc(userDocRef, {
+            id: uid,
+            name: displayName,
+            registered: true,
+          });
+        }
       }
       navigate("/log");
     } catch (error: any) {
