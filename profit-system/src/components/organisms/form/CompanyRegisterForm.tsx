7a75acdf334657cab916ac71c6880c82a6eb287a
Kenji <kkawakami391@gmail.com>
2023-07-20 17:03:52 +0900

    Add CompanyPostCode and CompanyPhone data to DB logic.

--- profit-system/src/components/organisms/form/CompanyRegisterForm.tsx
+++ profit-system/src/components/organisms/form/CompanyRegisterForm.tsx
@@ -1,8 +1,8 @@
 import { FormControl } from "@chakra-ui/react";
 import { FC, memo, useState } from "react";
 import { PrimaryFormItem } from "../../molecules/item/PrimaryFormItem";
-// import { PostNumFormItem } from "../../molecules/item/PostNumFormItem";
-// import { PhoneFormItem } from "../../molecules/item/PhoneFormItem";
+import { PostNumFormItem } from "../../molecules/item/PostNumFormItem";
+import { PhoneFormItem } from "../../molecules/item/PhoneFormItem";
 import { FormButtonContainer } from "../../molecules/container/FormButtonContainer";
 import { MainContentContainer } from "../../molecules/container/MainContentContainer";
 import { addDoc, collection } from "@firebase/firestore";
@@ -12,9 +12,12 @@ import { useNavigate } from "react-router";
 export const CompanyRegisterForm: FC = memo(() => {
   const navigate = useNavigate();
   const [companyName, setCompanyName] = useState("");
-  // const [companyPostCode, setCompanyPostCode] = useState("");
+  const [companyPostCode1, setCompanyPostCode1] = useState("");
+  const [companyPostCode2, setCompanyPostCode2] = useState("");
   const [companyAddress, setCompanyAddress] = useState("");
-  // const [companyPhone, setCompanyPhone] = useState("");
+  const [companyPhone1, setCompanyPhone1] = useState("");
+  const [companyPhone2, setCompanyPhone2] = useState("");
+  const [companyPhone3, setCompanyPhone3] = useState("");
 
   const handleCompanyName = (newValue: string) => {
     setCompanyName(newValue);
@@ -22,20 +25,37 @@ export const CompanyRegisterForm: FC = memo(() => {
   const handleCompanyAddress = (newValue: string) => {
     setCompanyAddress(newValue);
   };
+  const handleCompanyPostCode1 = (newValue: string) => {
+    setCompanyPostCode1(newValue);
+  };
+  const handleCompanyPostCode2 = (newValue: string) => {
+    setCompanyPostCode2(newValue);
+  };
+  const handleCompanyPhone1 = (newValue: string) => {
+    setCompanyPhone1(newValue);
+  };
+  const handleCompanyPhone2 = (newValue: string) => {
+    setCompanyPhone2(newValue);
+  };
+  const handleCompanyPhone3 = (newValue: string) => {
+    setCompanyPhone3(newValue);
+  };
 
   const onClickRegister = async () => {
-    const docRef = await addDoc(collection(db, "company"), {
+    await addDoc(collection(db, "company"), {
       name: companyName,
-      // postCode: companyPostCode,
+      postCode: `${companyPostCode1}-${companyPostCode2}`,
       address: companyAddress,
-      // phone: companyPhone,
+      phone: `${companyPhone1}-${companyPhone2}-${companyPhone3}`,
     });
-    console.log(docRef);
 
     setCompanyName("");
-    // setCompanyPostCode("");
+    setCompanyPostCode1("");
+    setCompanyPostCode2("");
     setCompanyAddress("");
-    // setCompanyPhone("");
+    setCompanyPhone1("");
+    setCompanyPhone2("");
+    setCompanyPhone3("");
 
     navigate("/companies_list");
   };
@@ -44,9 +64,16 @@ export const CompanyRegisterForm: FC = memo(() => {
     <FormControl>
       <MainContentContainer>
         <PrimaryFormItem text="顧客名" handleValueChange={handleCompanyName} />
-        {/* <PostNumFormItem /> */}
+        <PostNumFormItem
+          handleValueChange1={handleCompanyPostCode1}
+          handleValueChange2={handleCompanyPostCode2}
+        />
         <PrimaryFormItem text="住所" handleValueChange={handleCompanyAddress} />
-        {/* <PhoneFormItem /> */}
+        <PhoneFormItem
+          handleValueChange1={handleCompanyPhone1}
+          handleValueChange2={handleCompanyPhone2}
+          handleValueChange3={handleCompanyPhone3}
+        />
         <FormButtonContainer
           secondaryPx="10"
           ml="50px"
