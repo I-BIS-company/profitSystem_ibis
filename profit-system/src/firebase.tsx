87e935bd2e80f75476745a546c7e5bf435c1247e
Yasuharu Ueda <60287493+yasiharu@users.noreply.github.com>
2023-07-20 17:30:10 +0900

    chore:ゴミ掃除

--- profit-system/src/firebase.tsx
+++ profit-system/src/firebase.tsx
@@ -3,7 +3,6 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    // apiKey: import.meta.env.VITE_API_KEY,
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_PROJECT_ID,
