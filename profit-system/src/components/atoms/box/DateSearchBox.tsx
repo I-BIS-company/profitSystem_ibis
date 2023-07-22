f58deda61424c166ee72cfcb6b58223081189604
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-22 15:40:14 +0900

    map関数を使用し、日付のメンテナンス性を向上

--- profit-system/src/components/atoms/box/DateSearchBox.tsx
+++ profit-system/src/components/atoms/box/DateSearchBox.tsx
@@ -2,12 +2,13 @@ import { Box, Select } from "@chakra-ui/react";
 import { FC } from "react";
 
 export const DateSearchSelect: FC = () => {
+  const months = ["2023/06", "2023/07", "2023/08"];
   return (
     <Box>
       <Select bg="white" outline="none">
-        <option>2023/06</option>
-        <option>2023/07</option>
-        <option>2023/08</option>
+        {months.map((month) => (
+          <option>{month}</option>
+        ))}
       </Select>
     </Box>
   );
