406e289fc3cc1eef7292e01e8d100c0b8ff4dc58
Kenji <kkawakami391@gmail.com>
2023-07-24 15:11:44 +0900

    Add document id to onClick props for update.

--- profit-system/src/components/molecules/list/CompaniesTableTemplateList.tsx
+++ profit-system/src/components/molecules/list/CompaniesTableTemplateList.tsx
@@ -1,10 +1,40 @@
 import { Table, TableContainer, Tbody, Thead, Tr } from "@chakra-ui/react";
-import { memo, FC } from "react";
+import { memo, FC, useState, useEffect } from "react";
 import { TableHeadItem } from "../../atoms/item/TableHeadItem";
 import { TableBodyItem } from "../../atoms/item/TableBodyItem";
 import { EditItem } from "../EditItem";
+import { DocumentData, collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase";
+import { useNavigate } from "react-router-dom";
 
 export const CompaniesTableTemplateList: FC = memo(() => {
+  const navigate = useNavigate();
+  const [companyData, setCompanyData] = useState<DocumentData>([]);
+
+  useEffect(() => {
+    const getCompanyData = async () => {
+      const companyList: DocumentData[] = [];
+      const querySnapshot = await getDocs(collection(db, "company"));
+      querySnapshot.forEach((doc) => {
+        const dataId = {
+          id: doc.id,
+        };
+        companyList.push({ ...dataId, ...doc.data() });
+      });
+      setCompanyData(companyList);
+    };
+    getCompanyData();
+  }, []);
+  console.log(companyData);
+
+  const onClickEdit = (docId: string) => {
+    navigate("/companies_list/company_edit", {
+      state: {
+        docId: docId,
+      },
+    });
+  };
+
   return (
     <TableContainer>
       <Table variant="simple" border="none">
@@ -32,6 +62,15 @@ export const CompaniesTableTemplateList: FC = memo(() => {
             <TableBodyItem text="03-0000-0000" />
             <EditItem />
           </Tr>
+          {companyData.map((data: DocumentData) => (
+            <Tr fontSize="14" key={data.id}>
+              <TableBodyItem text={data.name} />
+              <TableBodyItem text={data.postCode} />
+              <TableBodyItem text={data.address} />
+              <TableBodyItem text={data.phone} />
+              <EditItem onClick={() => onClickEdit(data.id)} />
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </TableContainer>
