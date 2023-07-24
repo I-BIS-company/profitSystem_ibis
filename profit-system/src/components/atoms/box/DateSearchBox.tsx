fa526d466321faa990acc9505d20c14e976a0d44
kurokirishima-jiejie <aijie.div@gmail.com>
2023-07-24 15:45:20 +0900

    15時までの機能実装

--- profit-system/src/components/atoms/box/DateSearchBox.tsx
+++ profit-system/src/components/atoms/box/DateSearchBox.tsx
@@ -1,11 +1,21 @@
 import { Box, Select } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const DateSearchSelect: FC = () => {
   const months = ["2023/06", "2023/07", "2023/08"];
+  const [selectMonth, setSelectMonth] = useState(months[0]);
+
+  const handleChange = (e: any) => {
+    setSelectMonth(e.target.value);
+  };
   return (
     <Box>
-      <Select bg="white" outline="none">
+      <Select
+        bg="white"
+        outline="none"
+        value={selectMonth}
+        onChange={handleChange}
+      >
         {months.map((month) => (
           <option key={month}>{month}</option>
         ))}
