a6a72431c0f6a813114535fa3fe3b59f1f68075d
Kenji <kkawakami391@gmail.com>
2023-07-24 15:16:11 +0900

    Test obtain data from firebase with docId.

--- profit-system/src/components/organisms/form/CompanyEditForm.tsx
+++ profit-system/src/components/organisms/form/CompanyEditForm.tsx
@@ -1,12 +1,30 @@
 import { FormControl } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { FC, memo, useEffect, useState } from "react";
 import { FormButtonContainer } from "../../molecules/container/FormButtonContainer";
 import { MainContentContainer } from "../../molecules/container/MainContentContainer";
 import { PrimaryFormItem } from "../../molecules/item/PrimaryFormItem";
 import { PostNumFormItem } from "../../molecules/item/PostNumFormItem";
 import { PhoneFormItem } from "../../molecules/item/PhoneFormItem";
+import { useLocation } from "react-router-dom";
+import { DocumentData, doc, getDoc } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 export const CompanyEditForm: FC = memo(() => {
+  const location = useLocation();
+  const [companyEditData, setCompanyEditData] = useState<DocumentData>();
+  const docId = location.state.docId;
+  console.log(docId);
+
+  const companyDocRef = doc(db, "company", docId);
+  useEffect(() => {
+    const getCompanyEditData = async () => {
+      const companyDocSnap = await getDoc(companyDocRef);
+      console.log(companyDocSnap.data());
+      setCompanyEditData(companyDocSnap.data());
+    };
+    getCompanyEditData();
+  }, []);
+  console.log(companyEditData);
   return (
     <FormControl>
       <MainContentContainer>
