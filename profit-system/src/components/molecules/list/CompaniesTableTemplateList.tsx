3a367da965cfd6141285036e673f52a824d36945
Kenji <kkawakami391@gmail.com>
2023-07-25 10:41:39 +0900

    Remove hard-code lines

--- profit-system/src/components/molecules/list/CompaniesTableTemplateList.tsx
+++ profit-system/src/components/molecules/list/CompaniesTableTemplateList.tsx
@@ -48,20 +48,6 @@ export const CompaniesTableTemplateList: FC = memo(() => {
           </Tr>
         </Thead>
         <Tbody alignItems="center">
-          <Tr fontSize="14">
-            <TableBodyItem text="株式会社A" />
-            <TableBodyItem text="611-0000" />
-            <TableBodyItem text="東京都葛飾区〇〇町3丁目23-55" />
-            <TableBodyItem text="03-0000-0000" />
-            <EditItem />
-          </Tr>
-          <Tr fontSize="14">
-            <TableBodyItem text="株式会社B" />
-            <TableBodyItem text="611-0000" />
-            <TableBodyItem text="東京都品川区〇〇町5丁目23-69" />
-            <TableBodyItem text="03-0000-0000" />
-            <EditItem />
-          </Tr>
           {companyData.map((data: DocumentData) => (
             <Tr fontSize="14" key={data.id}>
               <TableBodyItem text={data.name} />
