b769b5a02ccf1f29a8f89da33543fe2885d8e4e3
Kenji <kkawakami391@gmail.com>
2023-07-27 12:28:21 +0900

    Add project name filtering function.

--- profit-system/src/components/pages/ProjectsList.tsx
+++ profit-system/src/components/pages/ProjectsList.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import { memo, FC, useState, useEffect } from "react";
+import { memo, FC, useState, useEffect, ChangeEvent } from "react";
 import { HeadLine } from "../atoms/HeadLine";
 import { MainScreenTopContainer } from "../molecules/container/MainScreenTopContainer";
 import { ContentBgTemplate } from "../molecules/container/ContentBgTemplateContainer";
@@ -13,7 +13,15 @@ import { useNavigate } from "react-router-dom";
 
 export const ProjectsList: FC = memo(() => {
   const navigate = useNavigate();
+  const [searchValue, setSearchValue] = useState("");
   const [projectData, setProjectData] = useState<ProjectDbType[]>([]);
+  const [filteredProjectList, setFilteredProjectList] = useState<
+    ProjectDbType[]
+  >([]);
+
+  const handleSearchText = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
 
   useEffect(() => {
     const getProjectData = async () => {
@@ -27,9 +35,19 @@ export const ProjectsList: FC = memo(() => {
         });
       });
       setProjectData(projectList);
+      setFilteredProjectList(projectList);
     };
     getProjectData();
   }, []);
+
+  const onClickFilter = () => {
+    const filteredProject = projectData.filter((project) => {
+      if (project.name.indexOf(searchValue) !== -1) {
+        return project;
+      }
+    });
+    setFilteredProjectList(filteredProject);
+  };
 
   const onClickRegisterPage = () => {
     navigate("/projects_list/project_register");
@@ -39,13 +57,17 @@ export const ProjectsList: FC = memo(() => {
     <>
       <HeadLine text="案件一覧" />
       <MainScreenTopContainer>
-        <PrimarySearchButton text="案件名" />
+        <PrimarySearchButton
+          text="案件名"
+          handleSearchText={handleSearchText}
+          onClick={onClickFilter}
+        />
         <Box>
           <IconButton text="案件を登録する" onClick={onClickRegisterPage} />
         </Box>
       </MainScreenTopContainer>
       <ContentBgTemplate>
-        <ProjectTableTemplateList projectList={projectData} />
+        <ProjectTableTemplateList projectList={filteredProjectList} />
       </ContentBgTemplate>
     </>
   );
